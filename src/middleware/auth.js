0384302d914ea1daf15afd5c2569332b50dac584
chavaleesing <chavalee.sing@gmail.com>
2020-07-23 15:22:21 +0700

    fix wrong path

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const logger = require('../../utils/logger');
+const logger = require('./../utils/logger');
 
 const tokenValidation = (req, res, nextHandler) => {
     logger.debug("auth middleware")
