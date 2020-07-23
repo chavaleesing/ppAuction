02075b8a69144ac50663fa24a3956a5fef97d1dd
chavaleesing <chavalee.sing@gmail.com>
2020-07-23 15:10:33 +0700

    add auth middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+const logger = require('../../utils/logger');
+
+const tokenValidation = (req, res, nextHandler) => {
+    logger.debug("auth middleware")
+    const accessToken = req.headers.authorization
+    const JWT_SECRET_KEY = 'superrrrSecret';
+    try {
+        const tokenPayload = jwt.verify(accessToken, JWT_SECRET_KEY);
+        res.locals.user = tokenPayload;
+        nextHandler();
+    } catch (error) {
+        res.status(401).send({"error": error.message});
+    }
+}
+
+module.exports = {
+    tokenValidation
+};
